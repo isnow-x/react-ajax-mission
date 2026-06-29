0eb9250803aef24bbdb3f6ba6bdc70b2a4783a12
isnow-x <cone2119@naver.com>
2026-06-29 10:23:39 +0900

    Implement routing and state management in App component; update Header styles for active links

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import "./App.css";
+import { Routes, Route } from "react-router";
+import { useState } from "react";
+import Home from "./pages/Home";
+import Layout from "./components/Layout";
 
 function App() {
-  return <></>;
+  const [posts, setPosts] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout loaded={loaded} />}>
+          <Route index element={<Home posts={posts} />} />
+          {/* <Route path="posts" element={<Posts posts={posts} />} />
+      <Route
+        path="posts/:id"
+        element={<PostDetail posts={} onDelete={}/>}
+      />
+      <Route path="*" element={<NotFound />}/> */}
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,16 +8,13 @@ export default function Header() {
         <Link to="/">Router Mission Blog</Link>
       </h1>
       <nav className={styles.nav}>
-        <NavLink to="/" className={({ isActive }) => (isActive ? styles.active : undefined)}>
+        <NavLink to="/" className={({ isActive }) => (isActive ? styles.open : undefined)}>
           Home
         </NavLink>
-        <NavLink to="/posts" className={({ isActive }) => (isActive ? styles.active : undefined)}>
+        <NavLink to="/posts" className={({ isActive }) => (isActive ? styles.open : undefined)}>
           Posts
         </NavLink>
-        <NavLink
-          to="/posts/new"
-          className={({ isActive }) => (isActive ? styles.active : undefined)}
-        >
+        <NavLink to="/posts/new" className={({ isActive }) => (isActive ? styles.open : undefined)}>
           Write
         </NavLink>
       </nav>
